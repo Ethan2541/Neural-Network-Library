beb6fa58d9a4d29dfafbb717c55f7f9bcfacef36
Ethan2541 <eluong41@gmail.com>
2024-04-03 14:45:55 +0200

    Linear, Non Linear, Sequential, AutoEncoder checked

--- pyldl/layers.py
+++ pyldl/layers.py
@@ -18,6 +18,7 @@ class Sequential(object):
         for i in reversed(range(len(self._modules))):
             self._modules[i].backward_update_gradient(self._inputs[i], delta)
             delta = self._modules[i].backward_delta(self._inputs[i], delta)
+        return delta
 
     def update_parameters(self, gradient_step=1e-3):
         for module in self._modules:
@@ -26,3 +27,28 @@ class Sequential(object):
     def zero_grad(self):
         for module in self._modules:
             module.zero_grad()
+
+
+class AutoEncoder(object):
+    def __init__(self, encoder, decoder):
+        self._encoder = encoder
+        self._decoder = decoder
+    
+    def __call__(self, *args, **kwds):
+        return self.forward(*args, **kwds)
+    
+    def forward(self, X):
+        return self._decoder(self._encoder(X))
+    
+    def backward(self, input, delta):
+        delta = self._decoder.backward(input, delta)
+        delta = self._encoder.backward(input, delta)
+        return delta
+
+    def update_parameters(self, gradient_step=1e-3):
+        self._encoder.update_parameters(gradient_step)
+        self._decoder.update_parameters(gradient_step)
+    
+    def zero_grad(self):
+        self._encoder.zero_grad()
+        self._decoder.zero_grad()
