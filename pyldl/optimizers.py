6e36e017fb51a2cd58e810b0d36f8fea325df2a1
Ethan2541 <eluong41@gmail.com>
2024-04-04 23:27:11 +0200

    Notebooks reworked to include train loss, random seed and train-test split

--- pyldl/optimizers.py
+++ pyldl/optimizers.py
@@ -1,5 +1,6 @@
 from tqdm import tqdm
 from pyldl.utils import create_minibatches
+import numpy as np
 
 class Optim(object):
     def __init__(self, net, loss, gradient_step=1e-3):
@@ -13,14 +14,21 @@ class Optim(object):
     def step(self, batch_x, batch_y):
         self._network.zero_grad()
         batch_yhat = self._network(batch_x)
+        loss_value = self._loss(batch_y, batch_yhat)
         delta = self._loss.backward(batch_y, batch_yhat)
         self._network.backward(batch_x, delta)
         self._network.update_parameters(self._gradient_step)
+        return loss_value
 
 
 def SGD(network, loss, X, y, batch_size=64, gradient_step=1e-3, n_iter=1000):
     batches = create_minibatches(X, y, batch_size)
     optimizer = Optim(network, loss, gradient_step)
+    losses = []
     for _ in tqdm(range(n_iter)):
+        batch_losses = []
         for (batch_X, batch_Y) in batches:
-            optimizer.step(batch_X, batch_Y)
+            loss_value = optimizer.step(batch_X, batch_Y)
+            batch_losses.append(loss_value)
+        losses.append(np.mean(batch_losses))
+    return losses
