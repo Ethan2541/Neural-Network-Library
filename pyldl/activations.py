Fixed Softmax and added Softmax and ReLU to the activation notebook

pyldl/activations.py
@@ -22,7 +22,7 @@ class Sigmoid(Activation):
 class Softmax(Activation):
     def forward(self, X):
         exp_X = np.exp(X - np.max(X, axis=-1, keepdims=True))
-        return exp_X / np.sum(exp_X, axis=1, keepdims=True)
+        return exp_X / np.sum(exp_X, axis=-1, keepdims=True)
     
     def backward_delta(self, input, delta):
         softmax = self(input)

pyldl/losses.py
@@ -19,7 +19,7 @@ class CrossEntropyLoss(Loss):
     
     def backward(self, y, yhat):
         exp_yhat = np.exp(yhat - np.max(yhat, axis=-1, keepdims=True))
-        return exp_yhat / np.sum(exp_yhat, axis=1, keepdims=True) - y
+        return exp_yhat / np.sum(exp_yhat, axis=-1, keepdims=True) - y
     
 
 class BCELoss(Loss):